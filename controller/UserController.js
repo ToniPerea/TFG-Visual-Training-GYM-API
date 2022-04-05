0a923877d5cec0840521966833f6140a6455fd7c
ToniPerea <apepa31@correo.ugr.es>
2022-04-05 20:43:57 +0200

    login controller and route

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,21 +1,63 @@
-const User = require("../model/UserSchema");
+const user = require("../model/UserSchema");
 const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const saltRounds = await bcrypt.genSalt();
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+    const saltRounds = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const newUser = new User({ username, password: passwordHash });
+    const newUser = new user({ email, password: passwordHash });
 
-  await newUser.save((err) => {
-    if (err) {
-      res.status(400).send("Error to register user");
-    } else {
-      res.status(200).send("User registered correctly!!!");
-    }
-  });
+    await newUser.save((err) => {
+      if (err) {
+        res.status(400).send("Error to register user");
+      } else {
+        res.status(200).send("User registered correctly!!!");
+      }
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      res.status(400).json({
+        msg: "Missing fields email or password",
+      });
+    }
+
+    const userEmail = await user.findOne({ email: email });
+
+    if (!userEmail) {
+      res.status(404).json({
+        msg: "Email not found",
+      });
+    }
+
+    const uncryptPass = await bcrypt.compare(password, userEmail.password);
+
+    if (!uncryptPass) {
+      res.status(401).json({
+        msg: "Incorrect password",
+      });
+    }
+
+    res.status(200).json({
+      msg: `Login was succesful with email ${userEmail.email}`,
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message,
+    });
+  }
+};
+
+module.exports = { register, login };
