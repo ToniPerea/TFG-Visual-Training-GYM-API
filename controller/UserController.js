f9d7bda8241e5a7c93acbc00cd2c85fb208666e8
ToniPerea <apepa31@gmail.com>
2022-06-25 17:45:30 +0200

    #3 Problem with Connect to Angular front

--- controller/UserController.js
+++ controller/UserController.js
@@ -2,62 +2,65 @@ const user = require("../model/UserSchema");
 const bcrypt = require("bcrypt");
 
 const register = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    const {email, password} = req.body;
 
     const saltRounds = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, saltRounds);
 
-    const newUser = new user({ email, password: passwordHash });
+    const newUser = new user({email, password: passwordHash});
 
-    await newUser.save((err) => {
-      if (err) {
-        res.status(400).send("Error to register user");
-      } else {
+    await newUser.save().then((userSaved) => {
+        console.log(userSaved);
         res.status(200).send("User registered correctly!!!");
-      }
-    });
-  } catch (err) {
-    res.status(500).json({
-      error: err.message,
-    });
-  }
-};
+    }).catch((error) => {
+        console.error(error)
+        res.status(404).send("User NOT registered correctly!!!");
+    })
+}
 
 const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const {email, password} = req.body;
+        let userEmail;
 
-    if (!email || !password) {
-      res.status(400).json({
-        msg: "Missing fields email or password",
-      });
+        if (!email || !password) {
+            res.status(400).json({
+                msg: "Missing fields email or password",
+            });
+        }
+
+        await user.findOne({email: email}).then((result) => {
+            userEmail = result;
+            if (!result) {
+                res.status(404).json({
+                    msg: "Email not found",
+                });
+            }
+        })
+
+
+        if (!userEmail) {
+            res.status(404).json({
+                msg: "Email not found",
+            });
+        }
+
+        const uncryptPass = await bcrypt.compare(password, userEmail.password);
+
+        if (!uncryptPass) {
+            res.status(401).json({
+                msg: "Incorrect password",
+            });
+        }
+
+        res.status(200).json({
+            msg: `Login was succesful with email ${userEmail.email}`,
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: err.message,
+        });
     }
-
-    const userEmail = await user.findOne({ email: email });
-
-    if (!userEmail) {
-      res.status(404).json({
-        msg: "Email not found",
-      });
-    }
-
-    const uncryptPass = await bcrypt.compare(password, userEmail.password);
-
-    if (!uncryptPass) {
-      res.status(401).json({
-        msg: "Incorrect password",
-      });
-    }
-
-    res.status(200).json({
-      msg: `Login was succesful with email ${userEmail.email}`,
-    });
-  } catch (err) {
-    res.status(500).json({
-      error: err.message,
-    });
-  }
 };
 
-module.exports = { register, login };
+module.exports = {register, login};
