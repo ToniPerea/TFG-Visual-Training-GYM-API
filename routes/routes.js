801cfe40ee920c67afbfd501b5c9f23489b6bf5c
ToniPerea <apepa31@gmail.com>
2022-06-27 19:44:38 +0200

    Create trainings on DB #6 - CRUD Methods por Trainings

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const { register, login } = require("../controller/UserController");
+const { add_training, get_training_by_id, update_training, delete_training} = require("../controller/TrainingController");
 
 const router = express.Router();
 
+// User Routes
 router.post("/register", register);
 router.post("/login", login);
+
+// Training Routes
+router.post("/training", add_training)
+router.get("/getTraining", get_training_by_id)
+router.put("/updateTraining", update_training)
+router.delete("/deleteTraining", delete_training)
 
 module.exports = router;
