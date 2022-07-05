fc26d31e4feba7587135b39d02e309f2dec78d3f
ToniPerea <apepa31@gmail.com>
2022-07-05 16:49:53 +0200

    #19 Trainings List Implementation

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,18 @@
 const express = require("express");
-const { register, login, update_user, get_user_by_email, users_list } = require("../controller/UserController");
-const { add_exercise, get_exercise_by_id, update_exercise, delete_exercise} = require("../controller/ExerciseController");
-const {add_training, get_training_by_id, update_training, delete_training} = require("../controller/TrainingController");
+const {register, login, update_user, get_user_by_email, users_list} = require("../controller/UserController");
+const {
+    add_exercise,
+    get_exercise_by_id,
+    update_exercise,
+    delete_exercise
+} = require("../controller/ExerciseController");
+const {
+    add_training,
+    get_training_by_id,
+    update_training,
+    delete_training,
+    trainings_list
+} = require("../controller/TrainingController");
 
 const router = express.Router();
 
@@ -23,5 +34,6 @@ router.post("/training", add_training)
 router.get("/getTraining", get_training_by_id)
 router.put("/updateTraining", update_training)
 router.delete("/deleteTraining", delete_training)
+router.get("/getTrainingsList", trainings_list)
 
 module.exports = router;
